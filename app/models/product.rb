7c672a28ab7eb38a0b039417e163309211e969aa
Demetris Tzionis <demetristz@hotmail.com>
2022-08-28 18:14:51 +0300

    updated banner and background color, updated form to simple_form

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,6 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
-  validates :title, presence: true
+  validates :title, presence: true, length: { maximum: 30 }
   validates :short_description, presence: true
   validates :description, presence: true
 end
